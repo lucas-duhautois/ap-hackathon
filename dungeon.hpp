10f2b65cdeee2f7e8e87830d8d3efa3f76ba1d82
Laurentin-wilhelm <laurentin-wilhelm.ahrend@etu.minesparis.psl.eu>
2025-01-31 16:04:53 +0100

    partiel

--- dungeon.hpp
+++ dungeon.hpp
@@ -11,15 +11,16 @@ public:
 std::vector<int> position;
 int Lx;
 int Ly;
-room(int init_Lx ,int init_Ly, std::vector<int>init_p, board map){};
+room(int init_Lx ,int init_Ly, std::vector<int>init_p, board &map);
 
 };
 
 class tile {
 public:
 std::vector<int> position;
-bool triggered = false;
 
-tile(std::vector<int>init_p, board map){};
+int direction = 0;
+
+tile(std::vector<int>init_p, board &map,int init_direction);
 
 };
