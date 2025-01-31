2e5cec94cc50b1b0e630e30920bb21e1f39157df
Laurentin-wilhelm <laurentin-wilhelm.ahrend@etu.minesparis.psl.eu>
2025-01-31 14:26:03 +0100

    dd

--- dungeon.cpp
+++ dungeon.cpp
@@ -2,7 +2,14 @@
 #include "dungeon.hpp"
 
 room::room(int init_Lx ,int init_Ly, std::vector<int>init_p, board map):Lx{init_Lx},Ly{init_Ly},position{init_p}
-{
+{int x;
+ int y;
+ for (x = 0; x<Lx; x++)
+  {for (y = 0; y < Ly; y++)
+  {
+    map(position[0] + x,position[1] + y) = 1;
+  }
+  }
 
 
 }
@@ -11,6 +18,15 @@ room::room(int init_Lx ,int init_Ly, std::vector<int>init_p, board map):Lx{init_
 
 tile::tile(std::vector<int>init_p, board map):position{init_p}
 {
-
+    map(position[0],position[1]) = 2;
+   int x;
+   int y;
+if( rand() % 10==5){
+   for (x = 0; x<10; x++)
+  {for (y = 0; y <10 ; y++)
+  {
+    map(position[0] + x,position[1] + y) = 1;
+  }
+  }}
 
 }
