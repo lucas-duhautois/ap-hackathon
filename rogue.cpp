1f083ac71033585bc78fb05f060c71874b8da4c4
Laurentin-wilhelm <laurentin-wilhelm.ahrend@etu.minesparis.psl.eu>
2025-01-31 12:45:32 +0100

    map

--- rogue.cpp
+++ rogue.cpp
@@ -17,6 +17,54 @@
 #include <functional>
 
 
-int main(){
-    return 0;
+
+
+
+class board
+{public:
+  int nx;
+  int ny;
+  int taille_cases;
+  std::vector<int> bg;
+
+
+  board(int init_nx, int init_ny, int taille_cases) : nx{init_nx}, ny{init_ny},taille_cases{taille_cases}
+  {
+  bg.resize(nx * ny, 0);
+  }
+};
+
+void startGame(board &map)
+{
+    sf::RenderWindow window(sf::VideoMode(map.nx * map.taille_cases, map.ny * map.taille_cases), "rogue");
+    while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            {window.close();
+            exit(1);}
+      
+    }
+  
+    
+    
+    
+    }
+     
+    
+  }
+
+
+int main()
+{
+  const int nx = 100;
+  const int ny = 50;
+  const int taille = 10;
+  board map(nx,ny,taille);
+
+
+  startGame(map);
+  return 0;
 }
