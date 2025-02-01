e80f1528568840b06e966028781693dc46c047d3
Gustave Caillaut <gustave.caillaut@etu.minesparis.psl.eu>
2025-02-01 16:41:54 +0100

    ajout des enemies

---
+++ enemies.cpp
@@ -0,0 +1,95 @@
+#include "enemies.hpp"
+#include "Hero.hpp"
+#include "rogue.hpp"
+
+EnemyType::EnemyType(int id, std::string nom, const std::string& texturePath, int hp, int damage, float hit_rate) : id{id}, nom{nom}, hp{hp}, damage{damage}, hit_rate{hit_rate} {
+        if (!texture.loadFromFile(texturePath)) {
+            std::cerr << "Erreur de chargement : " << texturePath << std::endl;
+        }
+        dicoEnemies[id] = this;
+
+    }
+
+
+
+
+Enemy::Enemy(EnemyType& type, std::vector<int> coord) : type{type}
+{
+
+    hp=type.hp;
+    sprite = new sf::Sprite;
+    sprite->setTexture(type.texture);
+    sprite->setPosition(coord[0], coord[1]);
+
+}
+
+void Enemy::combat(Hero& hero, sf::RenderWindow& window){
+        double succes;
+        int degats;
+        window.clear(sf::Color::Black);
+        sf::Text texte;
+        texte.setCharacterSize(10); 
+        texte.setFillColor(sf::Color::White); 
+        texte.setPosition(50, 50);
+        texte.setString("un très très méchant " + type.nom + " vous attaque !");
+        window.draw(texte);
+        window.display();
+        while (hp > 0 || hero.hp > 0){
+            
+            
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
+                window.clear(sf::Color::Black);
+                succes = randomDouble();
+                if (succes<0.7){
+                    degats = hero.strength + static_cast<Sword*>(hero.weapon)->damage;
+                    this->hp = this-> hp-degats;
+                    texte.setString("vous frappez et infligez " + std::to_string(degats) + "dégâts");
+                }
+                else{
+                    texte.setString("Vous échouez misérablement à toucher le " + type.nom);
+                }
+                window.draw(texte);
+                window.display();
+                sf::sleep(sf::milliseconds(800));
+                succes = randomDouble();
+
+                if (succes<0.7){
+                
+                    degats = type.damage;
+                    hero.damage(degats);
+                    texte.setString("le " + type.nom + " vous inflige "+ std::to_string(degats) + "dégâts");
+                }
+                else {
+                    texte.setString("le " + type.nom + " vous a raté");
+                }
+                
+                window.draw(texte);
+                window.display();
+                sf::sleep(sf::milliseconds(800));
+            }
+            
+            
+            
+
+        }
+        if (hp<=0){
+            texte.setString("Bravo ! Vous avez gagné. Le " + type.nom + " est mort.");
+        }
+        else{
+            window.clear(sf::Color::Black);
+            texte.setString("Vous êtes mort. Dommage");
+            texte.setCharacterSize(80); 
+            texte.setPosition(350, 400);
+        }
+        window.draw(texte);
+        window.display();
+
+    }
+
+
+
+
+void Enemy::print(sf::RenderWindow& fen){
+    fen.draw(*sprite);
+}
+
---
+++ enemies.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <array>
+#include <algorithm>
+#include <thread>
+#include "sys/ioctl.h"
+#include "termios.h"
+#include "stdio.h"
+#include "time.h"
+#include "stdlib.h"
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <chrono> 
+#include <set>
+#include <random>
+#include <functional>
+#include <map>
+
+class Hero;
+
+class EnemyType{
+    public:
+
+    static std::map<int, EnemyType*> dicoEnemies;
+    int id;
+    std::string nom;
+    sf::Texture texture;
+    int hp;
+    int damage;
+    float hit_rate;
+    
+    EnemyType(int id, std::string nom, const std::string& texturePath, int hp, int damage, float hit_rate);
+
+
+    
+};
+
+
+class Enemy{
+
+    EnemyType type;
+    int hp;
+    sf::Sprite* sprite;
+
+    Enemy(EnemyType& type, std::vector<int> coord);
+
+    void combat(Hero& hero, sf::RenderWindow& window);
+    void print(sf::RenderWindow& fen);
+};
